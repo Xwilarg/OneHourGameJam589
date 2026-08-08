b74cc08c5fd3506d052e5f59325b57371be58fb9
Xwilarg <xwilarg@protonmail.com>
2026-08-08 19:22:01 +0200

    Update gameplay

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -7,9 +7,22 @@ namespace OneHourGameJam589.Manager
         [SerializeField]
         private GameObject _blockPrefab;
 
+        private const int Size = 10;
+
         private void Awake()
         {
-            
+            for (int y = -1; y > -Size; y--)
+            {
+                for (int i = -Size; i <= Size; i++)
+                {
+                    Instantiate(_blockPrefab, new Vector3(i, y, 0f), Quaternion.identity);
+                }
+            }
+            for (int y = 5; y > -1; y--)
+            {
+                Instantiate(_blockPrefab, new Vector3(-Size, y, 0f), Quaternion.identity);
+                Instantiate(_blockPrefab, new Vector3(Size, y, 0f), Quaternion.identity);
+            }
         }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,7 +5,21 @@ namespace OneHourGameJam589.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField]
+        private float _speed;
+
         private float _movX;
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.linearVelocity = new Vector3(_movX * _speed, _rb.linearVelocity.y, 0f);
+        }
 
         public void OnMovement(InputAction.CallbackContext value)
         {
