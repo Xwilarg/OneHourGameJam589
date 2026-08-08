e77a48dd3017e9ce0f1a0ef1676ecd1924e5583d
Xwilarg <xwilarg@protonmail.com>
2026-08-08 19:13:44 +0200

    Add base scene

---
+++ Assets/Scripts/Manager/MapManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace OneHourGameJam589.Manager
+{
+    public class MapManager : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _blockPrefab;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace OneHourGameJam589.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private float _movX;
+
+        public void OnMovement(InputAction.CallbackContext value)
+        {
+            _movX = value.ReadValue<Vector2>().x;
+            if (_movX > .5) _movX = 1;
+            else if (_movX < -.5f) _movX = -1;
+            else _movX = 0;
+        }
+    }
+}
